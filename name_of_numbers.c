8436e3adab7b2fe488ffe0b7b90d783905512623
Dhivithana <30549440+Dhivithana@users.noreply.github.com>
2018-02-21 16:07:29 +0530

    Create name_of_numbers.c

---
+++ name_of_numbers.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int main()
+{
+int ch;
+printf("\n Enter your choice:");
+scanf('%d",&ch);
+switch(ch)
+{
+case 1:
+printf("\n One");
+break;
+case 2:
+printf("\n Two");
+break;
+case 3:
+printf("\n Three");
+break;
+case 4:
+printf("\n Four");
+break;
+case 5:
+printf("\n five");
+break;
+case 6:
+printf("\n six");
+break;
+case 7:
+printf("\n seven");
+break;
+case 8:
+printf("\n eight");
+break;
+case 9:
+printf("\n nine");
+break;
+case 10:
+printf("\n Ten");
+break;
+default:
+printf("\n Not in range");
+}
+return 0;
+}
+
+
+
+
+
+
+
