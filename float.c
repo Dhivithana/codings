c848a648d33d05a6db7ba7c1265d61b785e915d8
Dhivithana <30549440+Dhivithana@users.noreply.github.com>
2018-03-02 16:47:07 +0530

    Create float.c

---
+++ float.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+float n;
+int a;
+printf("\n Enter the number:");
+scanf("%f",&n);
+a=n+1;
+printf("\n The next highest number is:%d",a);
+return 0;
+}
