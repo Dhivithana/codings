cbdcb7c8213350da3125fbd73a6d481919a1d938
Dhivithana <30549440+Dhivithana@users.noreply.github.com>
2018-03-05 16:13:04 +0530

    Create composite.c

---
+++ composite.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int n,c=0,i;
+printf("\n Enter the number:");
+scanf("%d",&n);
+for(i=2;i<=n/2;i++)
+{
+if(n%i==0)
+{
+c++;
+}}
+if(c!=0)
+{
+printf("\n Yes");
+}
+else
+{
+printf("\n No");
+}
+return 0;
+}
